7988c624203971ccde4e744e4dc2439efa148e9e
Greg Ziebold <ziebold@gmail.com>
2014-08-06 14:00:01 -0600

    initial

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name twentyfortyeightApp
+ * @description
+ * # twentyfortyeightApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('twentyfortyeightApp', [
+    'ngCookies'
+  ]);
